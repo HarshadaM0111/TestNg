79565101ed5866616389f856f6481cbb9b626bb4
harshadaM0111 <harshadamane0111@gmail.com>
2026-05-21 09:20:03 +0530

    Learning Annotation TestNG, Priority

---
+++ selenium_automation/src/test/java/test/selenium_automation/PriorityCheck.java
@@ -0,0 +1,96 @@
+package test.selenium_automation;
+
+import org.testng.annotations.Test;
+
+public class PriorityCheck {
+
+//	@Test
+//	void display() {
+//		System.out.println("No Priority");
+//	}
+//	@Test(priority=0)
+//	void show() {
+//		System.out.println(" Zero Priority.");
+//	}
+//	@Test   // It taking method alphabets 
+//	void no() {
+//		System.out.println("No one Priority.");
+//	}
+//	
+//	
+//	@Test(priority=1)
+//	void one() {
+//		System.out.println("One Priority");
+//	}
+//	
+//	
+//	@Test(priority=2)
+//	void Two() {
+//		System.out.println("Two Priority");
+//	}
+//	
+//	@Test
+//	void nopri() {
+//		System.out.println("no 2 Priority");
+//	}
+//	
+//	@Test(priority = -1)
+//	void minusone() {
+//		System.out.println("minus one Priority");
+//	}
+//	
+//	@Test(priority = -3)
+//	void minusthree() {
+//		System.out.println("minus 3 Priority");
+//	}
+//	@Test(priority = 0)
+//	void minuszero() {
+//		System.out.println("minus 0 Priority");
+//	}
+//	
+//	@Test
+//	void noprio() {
+//		System.out.println("no Priority");
+//	}
+//	
+//	@Test(priority = -5)
+//	void minusefive() {
+//		System.out.println("minus  Priority");
+//	}
+//	
+//	@Test(priority = 0)
+//	void zero1() {
+//		System.out.println("zero Priority");
+//	}
+//	@Test(priority = 0)
+//	void zero2() {
+//		System.out.println("zero e Priority");
+//	}
+//	
+//	@Test(priority = 0)
+//	void tero4() {
+//		System.out.println("zero e2 Priority");
+//	}
+//	@Test(priority = 10)
+//	void ten() {
+//		System.out.println("ten Priority");
+//	}
+//	
+//	
+//	@Test(priority = -2)
+//	void methdf() {
+//		System.out.println("first minus  -2 Priority");
+//	}
+//	
+//	@Test(priority = -2)
+//	void fethodf2() {
+//		System.out.println("second -2 ");
+//	}
+	
+	@Test(priority = -2)
+	void hsrahda() {
+		System.out.println("third -2 Priority");
+	}
+}
+
+
